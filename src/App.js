038accd0c5c07acdb6d21a0e4cfa8428670317a8
Ananthalakshmi2514 <anithasunil.damineni@gmail.com>
2022-04-21 14:44:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+//import logo from './logo.svg';//
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<div className="header"> 
+  <div className="header1">logo</div>
+  <div className="header2">
+    <h1>Intro</h1>
+    <p>some introduction lines <br></br>about  website orcompany</p>
+  </div>
+  <div className="header3">
+  <div className="header3">link1</div>
+  <div className="header3">link2</div>
+  <div className="header3">link3</div>
+  <div className="header3">link4</div>
+  <div className="header3">link5</div>
+  </div>
+  <div className="header4">
+    featured project slider
+  </div>
+</div>
+<div className="content1">
+  <div className="content12">services</div>
+  <div className="content12" >services</div>
+  <div className="content12">services</div>
+</div>
+<div className="content2">
+  <div className="content21">recent work</div>
+  <div className="content21">recent work</div>
+  <div className="content21">recent work</div>
+</div>
+<div className="footer1">twitter feed</div>
+    </div>
+  );
+}
+
+export default App;
